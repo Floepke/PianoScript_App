6ffa48dfe076ef55dc216c00b48881c75af0f141
Pippin555 <harm.salomons@gmail.com>
2024-03-13 22:14:29 +0100

    larger button with larget icon on the test window, removed obsolete source code

--- test/icons_tester.py
+++ test/icons_tester.py
@@ -6,6 +6,7 @@ from PySide6.QtWidgets import QPushButton
 from PySide6.QtWidgets import QApplication
 from PySide6.QtWidgets import QMainWindow
 from PySide6.QtWidgets import QGridLayout
+from PySide6.QtCore import QSize
 
 from imports.icons.icons_data import image_dict
 
@@ -22,13 +23,15 @@ class SimpleTester:
 
         icon = get_icon('PopUp.png')
         root.setWindowIcon(icon)
-        root.setGeometry(250, 200, 120, 30)
+        root.setGeometry(100, 100, 240, 200)
 
         layout = QGridLayout()
         button = QPushButton('Next',
                              parent=root,
                              icon=get_icon('pianoscript.png'))
         button.setFixedWidth(120)
+        button.setIconSize(QSize(200, 200))
+        button.setFixedSize(QSize(240, 200))
         button.clicked.connect(self.button_click)
         layout.addWidget(button, 0, 0, 1, 1)
         root.setLayout(layout)
--- test/image_dictionary.py
+++
@@ -1,9 +0,0 @@
-#! python3.11
-# coding: utf8
-
-""" the dictionary with images in string format """
-
-# NOTE: this is an empty placeholder file, do not change the contents
-# see test/image_dict_test.py for more info
-#
-image_dict2 = {}
